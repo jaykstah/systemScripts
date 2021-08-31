e1b81ca6d22a07564ea1e0c889055cfeec00e471
jaykstah <jaykstah@gmail.com>
2021-08-31 14:49:10 -0700

    Updated performance.sh to allow returning to menu

--- performance.sh
+++ performance.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+clear
+echo "Appending performance to /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor"
 echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo performance > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 echo performance > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
 echo performance > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+echo "Success!"
+echo
+echo "Return to menu? (Y/N)"
+echo
+read -n1 -s -p "Selection:" input
+
+case $input in
+	"Y"|"y") ./menu.sh ;;
+	*) exit 0 ;;
+esac
